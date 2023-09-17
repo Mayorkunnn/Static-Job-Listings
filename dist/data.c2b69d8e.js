d41bc6ff718f7dc5f1ff06bfc61d1cce1d95f28b
Areola Kelvin Mayokun <areola.kelvin@stu.cu.edu.ng>
2023-09-17 23:13:59 +0100

    Building

---
+++ dist/data.c2b69d8e.js
@@ -0,0 +1,436 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"data.js":[function(require,module,exports) {
+/** @format */
+
+"use strict";
+
+var jobsObject = [{
+  id: 1,
+  company: "Photosnap",
+  logo: "./images/photosnap.svg",
+  new: true,
+  featured: true,
+  position: "Senior Frontend Developer",
+  role: "Frontend",
+  level: "Senior",
+  postedAt: "1d ago",
+  contract: "Full Time",
+  location: "USA Only",
+  languages: ["HTML", "CSS", "JavaScript"],
+  tools: []
+}, {
+  id: 2,
+  company: "Manage",
+  logo: "./images/manage.svg",
+  new: true,
+  featured: true,
+  position: "Fullstack Developer",
+  role: "Fullstack",
+  level: "Midweight",
+  postedAt: "1d ago",
+  contract: "Part Time",
+  location: "Remote",
+  languages: ["Python"],
+  tools: ["React"]
+}, {
+  id: 3,
+  company: "Account",
+  logo: "./images/account.svg",
+  new: true,
+  featured: false,
+  position: "Junior Frontend Developer",
+  role: "Frontend",
+  level: "Junior",
+  postedAt: "2d ago",
+  contract: "Part Time",
+  location: "USA Only",
+  languages: ["JavaScript"],
+  tools: ["React", "Sass"]
+}, {
+  id: 4,
+  company: "MyHome",
+  logo: "./images/myhome.svg",
+  new: false,
+  featured: false,
+  position: "Junior Frontend Developer",
+  role: "Frontend",
+  level: "Junior",
+  postedAt: "5d ago",
+  contract: "Contract",
+  location: "USA Only",
+  languages: ["CSS", "JavaScript"],
+  tools: []
+}, {
+  id: 5,
+  company: "Loop Studios",
+  logo: "./images/loop-studios.svg",
+  new: false,
+  featured: false,
+  position: "Software Engineer",
+  role: "Fullstack",
+  level: "Midweight",
+  postedAt: "1w ago",
+  contract: "Full Time",
+  location: "Worldwide",
+  languages: ["JavaScript"],
+  tools: ["Ruby", "Sass"]
+}, {
+  id: 6,
+  company: "FaceIt",
+  logo: "./images/faceit.svg",
+  new: false,
+  featured: false,
+  position: "Junior Backend Developer",
+  role: "Backend",
+  level: "Junior",
+  postedAt: "2w ago",
+  contract: "Full Time",
+  location: "UK Only",
+  languages: ["Ruby"],
+  tools: ["RoR"]
+}, {
+  id: 7,
+  company: "Shortly",
+  logo: "./images/shortly.svg",
+  new: false,
+  featured: false,
+  position: "Junior Developer",
+  role: "Frontend",
+  level: "Junior",
+  postedAt: "2w ago",
+  contract: "Full Time",
+  location: "Worldwide",
+  languages: ["HTML", "JavaScript"],
+  tools: ["Sass"]
+}, {
+  id: 8,
+  company: "Insure",
+  logo: "./images/insure.svg",
+  new: false,
+  featured: false,
+  position: "Junior Frontend Developer",
+  role: "Frontend",
+  level: "Junior",
+  postedAt: "2w ago",
+  contract: "Full Time",
+  location: "USA Only",
+  languages: ["JavaScript"],
+  tools: ["Vue", "Sass"]
+}, {
+  id: 9,
+  company: "Eyecam Co.",
+  logo: "./images/eyecam-co.svg",
+  new: false,
+  featured: false,
+  position: "Full Stack Engineer",
+  role: "Fullstack",
+  level: "Midweight",
+  postedAt: "3w ago",
+  contract: "Full Time",
+  location: "Worldwide",
+  languages: ["JavaScript", "Python"],
+  tools: ["Django"]
+}, {
+  id: 10,
+  company: "The Air Filter Company",
+  logo: "./images/the-air-filter-company.svg",
+  new: false,
+  featured: false,
+  position: "Front-end Dev",
+  role: "Frontend",
+  level: "Junior",
+  postedAt: "1mo ago",
+  contract: "Part Time",
+  location: "Worldwide",
+  languages: ["JavaScript"],
+  tools: ["React", "Sass"]
+}];
+},{}],"../../../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3730" + '/');
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      });
+
+      // Enable HMR for CSS by default.
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+  var parents = [];
+  var k, d, dep;
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+  return parents;
+}
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+  if (checkedAssets[id]) {
+    return;
+  }
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+}
+},{}]},{},["../../../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","data.js"], null)
+//# sourceMappingURL=/data.c2b69d8e.js.map
