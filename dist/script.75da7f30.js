a9db2aad5f684897eb6d1c6c52b137288e8fad20
Areola Kelvin Mayokun <areola.kelvin@stu.cu.edu.ng>
2023-09-20 16:09:50 +0100

    Debugging

--- dist/script.75da7f30.js
+++ dist/script.75da7f30.js
@@ -141,6 +141,147 @@ function _classPrivateMethodGet(receiver, privateSet, fn) { if (!privateSet.has(
 var mainContainer = document.querySelector("main");
 var screen = document.querySelector(".screen");
 var filterButtonsContainer = document.querySelector(".filter-buttons-container");
+var jobsObject = [{
+  id: 1,
+  company: "Photosnap",
+  logo: "./images/photosnap.svg",
+  new: true,
+  featured: true,
+  position: "Senior Frontend Developer",
+  role: "Frontend",
+  level: "Senior",
+  postedAt: "1d ago",
+  contract: "Full Time",
+  location: "USA Only",
+  languages: ["HTML", "CSS", "JavaScript"],
+  tools: []
+}, {
+  id: 2,
+  company: "Manage",
+  logo: "./images/manage.svg",
+  new: true,
+  featured: true,
+  position: "Fullstack Developer",
+  role: "Fullstack",
+  level: "Midweight",
+  postedAt: "1d ago",
+  contract: "Part Time",
+  location: "Remote",
+  languages: ["Python"],
+  tools: ["React"]
+}, {
+  id: 3,
+  company: "Account",
+  logo: "./images/account.svg",
+  new: true,
+  featured: false,
+  position: "Junior Frontend Developer",
+  role: "Frontend",
+  level: "Junior",
+  postedAt: "2d ago",
+  contract: "Part Time",
+  location: "USA Only",
+  languages: ["JavaScript"],
+  tools: ["React", "Sass"]
+}, {
+  id: 4,
+  company: "MyHome",
+  logo: "./images/myhome.svg",
+  new: false,
+  featured: false,
+  position: "Junior Frontend Developer",
+  role: "Frontend",
+  level: "Junior",
+  postedAt: "5d ago",
+  contract: "Contract",
+  location: "USA Only",
+  languages: ["CSS", "JavaScript"],
+  tools: []
+}, {
+  id: 5,
+  company: "Loop Studios",
+  logo: "./images/loop-studios.svg",
+  new: false,
+  featured: false,
+  position: "Software Engineer",
+  role: "Fullstack",
+  level: "Midweight",
+  postedAt: "1w ago",
+  contract: "Full Time",
+  location: "Worldwide",
+  languages: ["JavaScript"],
+  tools: ["Ruby", "Sass"]
+}, {
+  id: 6,
+  company: "FaceIt",
+  logo: "./images/faceit.svg",
+  new: false,
+  featured: false,
+  position: "Junior Backend Developer",
+  role: "Backend",
+  level: "Junior",
+  postedAt: "2w ago",
+  contract: "Full Time",
+  location: "UK Only",
+  languages: ["Ruby"],
+  tools: ["RoR"]
+}, {
+  id: 7,
+  company: "Shortly",
+  logo: "./images/shortly.svg",
+  new: false,
+  featured: false,
+  position: "Junior Developer",
+  role: "Frontend",
+  level: "Junior",
+  postedAt: "2w ago",
+  contract: "Full Time",
+  location: "Worldwide",
+  languages: ["HTML", "JavaScript"],
+  tools: ["Sass"]
+}, {
+  id: 8,
+  company: "Insure",
+  logo: "./images/insure.svg",
+  new: false,
+  featured: false,
+  position: "Junior Frontend Developer",
+  role: "Frontend",
+  level: "Junior",
+  postedAt: "2w ago",
+  contract: "Full Time",
+  location: "USA Only",
+  languages: ["JavaScript"],
+  tools: ["Vue", "Sass"]
+}, {
+  id: 9,
+  company: "Eyecam Co.",
+  logo: "./images/eyecam-co.svg",
+  new: false,
+  featured: false,
+  position: "Full Stack Engineer",
+  role: "Fullstack",
+  level: "Midweight",
+  postedAt: "3w ago",
+  contract: "Full Time",
+  location: "Worldwide",
+  languages: ["JavaScript", "Python"],
+  tools: ["Django"]
+}, {
+  id: 10,
+  company: "The Air Filter Company",
+  logo: "./images/the-air-filter-company.svg",
+  new: false,
+  featured: false,
+  position: "Front-end Dev",
+  role: "Frontend",
+  level: "Junior",
+  postedAt: "1mo ago",
+  contract: "Part Time",
+  location: "Worldwide",
+  languages: ["JavaScript"],
+  tools: ["React", "Sass"]
+}];
 var _filterJobs = /*#__PURE__*/new WeakSet();
 var _renderJobs = /*#__PURE__*/new WeakSet();
 var Jobs = /*#__PURE__*/function () {
@@ -295,7 +436,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "11941" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13615" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- script.js
+++ script.js
@@ -4,160 +4,287 @@
 
 const mainContainer = document.querySelector("main");
 const screen = document.querySelector(".screen");
+const asideContainer = document.querySelector("aside");
 const filterButtonsContainer = document.querySelector(
   ".filter-buttons-container"
 );
 
+const jobsObject = [
+  {
+    id: 1,
+    company: "Photosnap",
+    logo: "./images/photosnap.svg",
+    new: true,
+    featured: true,
+    position: "Senior Frontend Developer",
+    role: "Frontend",
+    level: "Senior",
+    postedAt: "1d ago",
+    contract: "Full Time",
+    location: "USA Only",
+    languages: ["HTML", "CSS", "JavaScript"],
+    tools: [],
+  },
+  {
+    id: 2,
+    company: "Manage",
+    logo: "./images/manage.svg",
+    new: true,
+    featured: true,
+    position: "Fullstack Developer",
+    role: "Fullstack",
+    level: "Midweight",
+    postedAt: "1d ago",
+    contract: "Part Time",
+    location: "Remote",
+    languages: ["Python"],
+    tools: ["React"],
+  },
+  {
+    id: 3,
+    company: "Account",
+    logo: "./images/account.svg",
+    new: true,
+    featured: false,
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    postedAt: "2d ago",
+    contract: "Part Time",
+    location: "USA Only",
+    languages: ["JavaScript"],
+    tools: ["React", "Sass"],
+  },
+  {
+    id: 4,
+    company: "MyHome",
+    logo: "./images/myhome.svg",
+    new: false,
+    featured: false,
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    postedAt: "5d ago",
+    contract: "Contract",
+    location: "USA Only",
+    languages: ["CSS", "JavaScript"],
+    tools: [],
+  },
+  {
+    id: 5,
+    company: "Loop Studios",
+    logo: "./images/loop-studios.svg",
+    new: false,
+    featured: false,
+    position: "Software Engineer",
+    role: "Fullstack",
+    level: "Midweight",
+    postedAt: "1w ago",
+    contract: "Full Time",
+    location: "Worldwide",
+    languages: ["JavaScript"],
+    tools: ["Ruby", "Sass"],
+  },
+  {
+    id: 6,
+    company: "FaceIt",
+    logo: "./images/faceit.svg",
+    new: false,
+    featured: false,
+    position: "Junior Backend Developer",
+    role: "Backend",
+    level: "Junior",
+    postedAt: "2w ago",
+    contract: "Full Time",
+    location: "UK Only",
+    languages: ["Ruby"],
+    tools: ["RoR"],
+  },
+  {
+    id: 7,
+    company: "Shortly",
+    logo: "./images/shortly.svg",
+    new: false,
+    featured: false,
+    position: "Junior Developer",
+    role: "Frontend",
+    level: "Junior",
+    postedAt: "2w ago",
+    contract: "Full Time",
+    location: "Worldwide",
+    languages: ["HTML", "JavaScript"],
+    tools: ["Sass"],
+  },
+  {
+    id: 8,
+    company: "Insure",
+    logo: "./images/insure.svg",
+    new: false,
+    featured: false,
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    postedAt: "2w ago",
+    contract: "Full Time",
+    location: "USA Only",
+    languages: ["JavaScript"],
+    tools: ["Vue", "Sass"],
+  },
+  {
+    id: 9,
+    company: "Eyecam Co.",
+    logo: "./images/eyecam-co.svg",
+    new: false,
+    featured: false,
+    position: "Full Stack Engineer",
+    role: "Fullstack",
+    level: "Midweight",
+    postedAt: "3w ago",
+    contract: "Full Time",
+    location: "Worldwide",
+    languages: ["JavaScript", "Python"],
+    tools: ["Django"],
+  },
+  {
+    id: 10,
+    company: "The Air Filter Company",
+    logo: "./images/the-air-filter-company.svg",
+    new: false,
+    featured: false,
+    position: "Front-end Dev",
+    role: "Frontend",
+    level: "Junior",
+    postedAt: "1mo ago",
+    contract: "Part Time",
+    location: "Worldwide",
+    languages: ["JavaScript"],
+    tools: ["React", "Sass"],
+  },
+];
+
 class Jobs {
   jobs = [];
-  uniqueArr = [];
-  arr = [];
+  #uniqueArr = [];
+  #arr = new Set();
   constructor() {
     this.jobs = JSON.parse(JSON.stringify(jobsObject)) || [];
-    this.jobs.forEach((job) => {
-      job.filterInfo = [job.role, job.level, ...job.languages, ...job.tools];
-    });
-    this.#renderJobs(this.jobs);
+    this.#renderJobs();
+    // const filterInfo = [this.jobs.role, this.jobs.level, ...this.jobs.languages, ...this.jobs.tools];
     mainContainer.addEventListener("click", (e) => {
       const target = e.target;
-
-      // Check if the clicked element has the 'btn' class
       if (target.classList.contains("btn")) {
         this.#filterJobs(target);
       }
-
-      this.removeFilterJobs(this.arr);
     });
-  }
 
-  removeFilter(filterToRemove) {
-    const index = this.uniqueArr.indexOf(filterToRemove);
-    if (index !== -1) {
-      this.uniqueArr.splice(index, 1);
-    }
+    asideContainer.addEventListener("click", (e) => {
+      const target = e.target;
+
+      if (target.classList.contains("close")) {
+        this.#removeFilterJobs(target.parentElement);
+      }
+    });
   }
 
   #filterJobs(clicked) {
-    this.arr.push(clicked.textContent);
-    this.uniqueArr = [...new Set(this.arr)];
-    this.displayOnScreen();
+    this.#arr.add(clicked.textContent);
+    this.#uniqueArr = [...this.#arr];
+    this.#displayOnScreen();
+
     if (screen.children.length > 0) screen.classList.remove("hidden");
 
-    // Filter jobs based on selected filters
     const filteredJobs = this.jobs.filter((job) => {
-      return this.uniqueArr.every((filter) => job.filterInfo.includes(filter));
+      return this.#uniqueArr.every((el) => job.filterInfo.includes(el));
     });
 
-    // Clear the main container before rendering the filtered jobs
     mainContainer.innerHTML = "";
     this.#renderJobs(filteredJobs);
   }
 
-  removeFilterJobs(arr) {
-    const closeButtons = document.querySelectorAll(".close");
-    closeButtons.forEach((closeButton) => {
-      closeButton.addEventListener("click", (e) => {
-        const filterToRemove = e.target.parentElement.textContent.trim();
-
-        // Remove the filter from arr
-        const index = arr.indexOf(filterToRemove);
-        if (index !== -1) {
-          arr.splice(index, 1);
-        }
-
-        // Remove the filter button
-        filterButtonsContainer.removeChild(e.target.parentElement);
-
-        // Reapply filters
-        const filteredJobs = this.jobs.filter((job) => {
-          return arr.every((filter) => job.filterInfo.includes(filter));
-        });
-
-        // Clear the main container before rendering the filtered jobs
-        mainContainer.innerHTML = "";
-        this.#renderJobs(filteredJobs);
-        if (arr.length === 0) {
-          // If no filters left, hide the screen
-          screen.classList.add("hidden");
-        }
-      });
-    });
-  }
-
-  displayOnScreen() {
+  #displayOnScreen() {
     let html = "";
-    this.uniqueArr.forEach((fil) => {
-      html += `<button class="btn btn__close" type="button" title=${fil}>
-          ${fil}<img
+    this.#arr.forEach((el) => {
+      html += `<button class="btn btn__close" type="button" title=${el} >
+          ${el} <img
             class="close"
             src="./images/icon-remove.svg"
             alt=""
-          /></button>`;
+          /></button
+        >`;
     });
 
-    // Insert the filter buttons into the filterButtonsContainer
     filterButtonsContainer.innerHTML = html;
   }
 
+  #removeFilterJobs(clicked) {
+    const filterToRemove = clicked.textContent.trim();
+
+    // Delete from Set
+    this.#arr.delete(filterToRemove);
+
+    // Update Array
+    this.#uniqueArr = [...this.#arr];
+
+    filterButtonsContainer.removeChild(clicked);
+
+    const filteredJobs = this.jobs.filter((job) => {
+      return this.#uniqueArr.every((el) => job.filterInfo.includes(el));
+    });
+
+    mainContainer.innerHTML = "";
+    this.#renderJobs(filteredJobs);
+
+    if (this.#arr.size === 0) screen.classList.add("hidden");
+  }
+
   #renderJobs(jobs = this.jobs) {
-    // if (jobs.length === 0) mainContainer.textContent = " No Jobs Available";
-    if (jobs) {
-      mainContainer.innerHTML = "";
+    if (!jobs) return;
+    jobs.forEach((job) => {
+      job.filterInfo = [job.role, job.level, ...job.languages, ...job.tools];
+      // job.push(filterInfo);
 
-      jobs.forEach((job) => {
-        const filterInfo = [
-          job.role,
-          job.level,
-          ...job.languages,
-          ...job.tools,
-        ];
-        const jobsFil = function () {
-          const jobContainers = document.querySelectorAll(".job");
+      const displayFilters = () => {
+        const jobsContainer = document.querySelectorAll(".job");
 
-          const filter = document.createElement("div");
-          filter.classList.add("filter");
+        const filter = document.createElement("div");
+        filter.classList.add("filter");
 
-          filterInfo.forEach((fil) => {
-            const html = `
-            <button class="btn" type="button" title=${fil}>${fil}</button>
-            `;
+        job.filterInfo.forEach((fil) => {
+          const html = `<button class="btn" type="button" title=${fil}>${fil}</button>`;
+          filter.insertAdjacentHTML("beforeend", html);
+        });
 
-            filter.insertAdjacentHTML("beforeend", html);
-          });
-          jobContainers.forEach((jobContainer) => {
-            jobContainer.appendChild(filter);
-            if (job.new) jobContainer.classList.add("new__content");
-            if (job.featured) jobContainer.classList.add("featured__content");
-          });
-        };
+        jobsContainer.forEach((jobContainer) => {
+          jobContainer.append(filter);
+          if (job.new) jobContainer.classList.add("new__content");
+          if (job.featured) jobContainer.classList.add("featured__content");
+        });
+      };
 
-        const html = `
-        <section class="job" id= ${job.id}>
-            <div class="main">
-            <img class="job__image" src= ${job.logo} alt="" />
-            <div class="content">
-                <div class="content__top">
-                <p>${job.company}</p>
-                <p class="tab new">NEW!</p>
-                <p class="tab featured">FEATURED</p>
-                </div>
-                <h3 class="content__middle">${job.position}</h3>
-                <div class="content__bottom">
-                <p>${job.postedAt} </p>
-                <p>.</p>
-                <p>${job.contract} </p>
-                <p>.</p>
-                <p>${job.location} </p>
-                </div>
+      const html = `<section class="job">
+        <div class="main">
+          <img class="job__image" src=${job.logo} alt=${job.company} />
+          <div class="content">
+            <div class="content__top">
+              <p>${job.company} </p>
+              <p class="tab new">NEW!</p>
+              <p class="tab featured">FEATURED</p>
             </div>
+            <h3 class="content__middle">${job.position}</h3>
+            <div class="content__bottom">
+              <p>${job.postedAt}</p>
+              <p>.</p>
+              <p>${job.contract}</p>
+              <p>.</p>
+              <p>${job.location}</p>
             </div>
-        </section> 
-            `;
+          </div>
+        </div>
+      </section>`;
 
-        mainContainer.insertAdjacentHTML("beforeend", html);
-        jobsFil();
-      });
-    }
+      mainContainer.insertAdjacentHTML("beforeend", html);
+      displayFilters();
+    });
   }
 }
 
